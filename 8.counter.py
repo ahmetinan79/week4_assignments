12ed8594b1231657f61200945ec5fd16804c8bc7
ahmetinan79 <60386232+ahmetinan79@users.noreply.github.com>
2020-02-13 22:27:22 +0100

    Update 8.counter.py

--- 8.counter.py
+++ 8.counter.py
@@ -3,7 +3,7 @@ def counter(text):
     # We use list comprehension with isupper and islower to split lower and upper letters into two lists.
     s_upper = [i for i in text if i.isupper()]
     s_lower = [i for i in text if i.islower()]
-    return f"smaller letter: {len(s_upper)}\nupper letter: {len(s_lower)}"
+    return f"smaller letter: {len(s_lower)}\nupper letter: {len(s_upper)}"
     # We find the number of lower and upper letters with len function.
 
 
